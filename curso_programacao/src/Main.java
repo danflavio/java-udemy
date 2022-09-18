b9943aba269a70de35abddd324085c6a6dfe968b
Daniel Flavio <danflavio@gmail.com>
2022-09-18 10:48:40 -0300

    Execução da aula de entrada de dados em Java

--- curso_programacao/src/Main.java
+++ curso_programacao/src/Main.java
@@ -10,35 +10,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		int y = 32;
-		double x = 10.35784;
-		String nome = "Maria";
-		int idade = 31;
-		double renda = 4000.00;
-		SaidaEntradaDados objetoSaidaEntradaDados = new SaidaEntradaDados();
-				
-
 		
-		System.out.println("Ola mundo!");
-		System.out.println("Bom dia!");
-		System.out.println(y);
-		
-		System.out.printf("%.2f%n", x);
-		System.out.printf("%.4f%n", x);
-		
-		//Locale.setDefault(Locale.US);
-		
-		System.out.printf("%.2f%n", x);
-		System.out.printf("%.4f%n", x);
-		
-		System.out.println("Custou " + x);
-		
-		System.out.printf("RESULTADO = %.2f metros%n", x);
-
-		System.out.printf("%s tem %d anos e ganha %.2f reais mensais. %n", nome, idade, renda);
-		
-		System.out.println("-----------------------\n\n\n");
-		objetoSaidaEntradaDados.printInfo();
 		
 	}
 
